5afd33499ba6cac4198f9b0bc829019d76fcc788
Ian Danforth <iandanforth@gmail.com>
2019-02-15 16:10:51 -0800

    Remove unused reward code. Clarify signal transformation

--- muscledagents/envs/mujoco/muscled_ant.py
+++ muscledagents/envs/mujoco/muscled_ant.py
@@ -1,5 +1,4 @@
 import numpy as np
-from mujoco_py import MujocoException
 from .base_muscled_env import BaseMuscledEnv
 
 
@@ -21,37 +20,17 @@ class MuscledAntEnv(BaseMuscledEnv):
         for i, muscle in enumerate(self.muscles):
             output = muscle.step(a[i], 0.002 * self.frame_skip)
             fatigues.append(muscle.get_peripheral_fatigue())
-            output = -1 * output
             outputs.append(output)
         self.muscle_fatigues = np.array(fatigues)
 
         # Step the physics sim
-        outputs = np.array(outputs)
-
-        # This model can randomly become unstable
-        # Until this can be reproduced reliably this hack will
-        # have to do.
-        unstable = 0
-        try:
-            self.do_simulation(outputs, self.frame_skip)
-        except MujocoException as e:
-            print(e)
-            unstable = 1
+        actuator_inputs = np.array(outputs) * -1   # Invert for MuJoCo
+        self.do_simulation(actuator_inputs, self.frame_skip)
 
         # Calculate reward and see if we're done
         xposafter = self.get_body_com("torso")[0]
-        forward_reward = (xposafter - xposbefore)/self.dt
-        ctrl_cost = .5 * np.square(a).sum()
-        # cfrc_ext is "com-based external force on body         (nbody x 6)"
-        # I don't know what this means or why it is scaled
-        # to this level.
-        contact_cost = 0.5 * 0.001 * np.sum(
-            np.square(np.clip(self.sim.data.cfrc_ext, -1, 1))
-        )
-        survive_reward = 1.0
-        reward = forward_reward - ctrl_cost - contact_cost + survive_reward
-        # TODO: Remove this
-        reward = forward_reward - unstable
+        forward_reward = (xposafter - xposbefore) / self.dt
+        reward = forward_reward
 
         # State is a flattened combination of qpos and qvel
         # Here it has 29 items
@@ -62,17 +41,11 @@ class MuscledAntEnv(BaseMuscledEnv):
         #   - The agent is not too low (z <= 0.2) or too high z >=1.0
         notdone = np.isfinite(state).all() and state[2] >= 0.2 and state[2] <= 1.0
         done = not notdone
-        if unstable != 0:
-            done = True
 
         # Get environment observations
         ob = self._get_obs()
 
-        return ob, reward, done, dict(
-            reward_forward=forward_reward,
-            reward_ctrl=-ctrl_cost,
-            reward_contact=-contact_cost,
-            reward_survive=survive_reward)
+        return ob, reward, done, dict(reward_forward=forward_reward)
 
     def _get_obs(self):
         return np.concatenate([
@@ -83,7 +56,11 @@ class MuscledAntEnv(BaseMuscledEnv):
         ])
 
     def reset_model(self):
-        qpos = self.init_qpos + self.np_random.uniform(size=self.model.nq, low=-.1, high=.1)
+        qpos = self.init_qpos + self.np_random.uniform(
+            size=self.model.nq,
+            low=-0.1,
+            high=0.1
+        )
         qvel = self.init_qvel + self.np_random.randn(self.model.nv) * .1
         self.set_state(qpos, qvel)
         self._reset_muscles(self.muscle_count)
